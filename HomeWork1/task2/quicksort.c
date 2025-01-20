40c296cf5cd4bd1329244f41f32f02fd7e49bd88
Jonathan <jcullgert@hotmail.se>
2025-01-20 15:42:54 +0100

    start of task2 for homework 1

--- HomeWork1/task2/quicksort.c
+++ HomeWork1/task2/quicksort.c
@@ -11,6 +11,86 @@
 #define MAXSIZE 10000  /* maximum matrix size */
 #define MAXWORKERS 10   /* maximum number of workers */
 
+
+double read_timer() {
+    static bool initialized = false;
+    static struct timeval start;
+    struct timeval end;
+    if( !initialized )
+    {
+        gettimeofday( &start, NULL );
+        initialized = true;
+    }
+    gettimeofday( &end, NULL );
+    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+//quicksort alogritm
+
+void swap(int* a, int* b);
+
+// Partition function
+int partition(int arr[], int low, int high) {
+    
+    // Choose the pivot
+    int pivot = arr[high];
+    
+    // Index of smaller element and indicates 
+    // the right position of pivot found so far
+    int i = low - 1;
+
+    // Traverse arr[low..high] and move all smaller
+    // elements to the left side. Elements from low to 
+    // i are smaller after every iteration
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    
+    // Move pivot after smaller elements and
+    // return its position
+    swap(&arr[i + 1], &arr[high]);  
+    return i + 1;
+}
+
+// The QuickSort function implementation
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+		
+        
+        // pi is the partition return index of pivot
+        int pi = partition(arr, low, high);
+
+        // Recursion calls for smaller elements
+        // and greater or equals elements
+		//när vi kallar på quicksort vi vi göra det med en thread
+		//pthread_create(&workerid, &attr, quickSort, (void *) arr; low; pi - 1);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+
+		//pthread_join()
+    }
+}
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
 //initialisera variabler för program
 
 //gör en quicksort algoritm
