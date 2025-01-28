c879e11c94006a690935e283cbce111df6d1fd07
Jonathan <jcullgert@hotmail.se>
2025-01-28 16:49:52 +0100

    fixed forgotten header from the file that the teacher provided us >:(

--- HomeWork2/task1/matrixSum.c
+++ HomeWork2/task1/matrixSum.c
@@ -2,43 +2,52 @@
 /* matrix summation using OpenMP
 usage with gcc (version 4.2 or higher required):
 gcc -O -fopenmp -o matrixSum-openmp matrixSum-openmp.c
+gcc -O -fopenmp -o matrixSum matrixSum.c
 ./matrixSum-openmp size numWorkers
 */
 #include <omp.h>
 double start_time, end_time;
 #include <stdio.h>
+#include <stdlib.h>
+
 #define MAXSIZE 10000 /* maximum matrix size */
-#define MAXWORKERS 8 /* maximum number of workers */
+#define MAXWORKERS 8  /* maximum number of workers */
 int numWorkers;
 int size;
 int matrix[MAXSIZE][MAXSIZE];
 void *Worker(void *);
 /* read command line, initialize, and create threads */
-int main(int argc, char *argv[]) {
-int i, j, total=0;
-/* read command line args if any */
-size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
-numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
-if (size > MAXSIZE) size = MAXSIZE;
-if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
-omp_set_num_threads(numWorkers);
-/* initialize the matrix */
-for (i = 0; i < size; i++) {
-// printf("[ ");
-for (j = 0; j < size; j++) {
-matrix[i][j] = rand()%99;
-// printf(" %d", matrix[i][j]);
-}
-// printf(" ]\n");
-}
-start_time = omp_get_wtime();
-#pragma omp parallel for reduction (+:total) private(j)
-for (i = 0; i < size; i++)
-for (j = 0; j < size; j++){
-total += matrix[i][j];
-}
-// implicit barrier
-end_time = omp_get_wtime();
-printf("the total is %d\n", total);
-printf("it took %g seconds\n", end_time - start_time);
+int main(int argc, char *argv[])
+{
+    int i, j, total = 0;
+    /* read command line args if any */
+    size = (argc > 1) ? atoi(argv[1]) : MAXSIZE;
+    numWorkers = (argc > 2) ? atoi(argv[2]) : MAXWORKERS;
+    if (size > MAXSIZE)
+        size = MAXSIZE;
+    if (numWorkers > MAXWORKERS)
+        numWorkers = MAXWORKERS;
+    omp_set_num_threads(numWorkers);
+    /* initialize the matrix */
+    for (i = 0; i < size; i++)
+    {
+        // printf("[ ");
+        for (j = 0; j < size; j++)
+        {
+            matrix[i][j] = rand() % 99;
+            // printf(" %d", matrix[i][j]);
+        }
+        // printf(" ]\n");
+    }
+    start_time = omp_get_wtime();
+#pragma omp parallel for reduction(+ : total) private(j)
+    for (i = 0; i < size; i++)
+        for (j = 0; j < size; j++)
+        {
+            total += matrix[i][j];
+        }
+    // implicit barrier
+    end_time = omp_get_wtime();
+    printf("the total is %d\n", total);
+    printf("it took %g seconds\n", end_time - start_time);
 }
