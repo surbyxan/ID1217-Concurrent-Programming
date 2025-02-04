86f6e413edbac563e9fe04d360ff711a75501a45
Jonathan <jcullgert@hotmail.se>
2025-02-04 14:06:59 +0100

    done with task 2 in hw3

---
+++ HomeWork3/task2/thebearandbees.c
@@ -0,0 +1,91 @@
+/*Terminate the program with ctrl + c*/
+/*Compile and execute: */
+//gcc thebearandbees.c -o thebearandbees
+// ./thebearandbees
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sched.h>
+
+#define WORKERS 6 //cannot be less than 2
+#define SHARED 1
+#define H 50 //honey portions
+
+sem_t serve; //full semaphore
+sem_t eat; //empty semaphore
+sem_t mutex; //waiting babies
+
+int honeypot = 0;
+
+//parent function
+void *bees(void *arg){
+    //vi lägga till honey i honeypot om den inte är full.
+    //om den är full sätter vi igång väckarklockan och väcker björnen
+    int argInt = *(int*) arg;
+
+    while(true){
+            
+        sem_wait(&eat); //den får den tomma semaphore
+        printf("Bees %d: Nothings like a hard days of work, honey is my life\n Honey = %d\n", argInt, honeypot);
+        honeypot += 3; //jobbar hårt och fyller på honey
+
+        if(honeypot > H)
+            honeypot = H;
+        
+        if(honeypot == H){
+            printf("ʕ•ᴥ•ʔ: Nothings like a good nap! I hope there is some honey in that pot! :P\n");
+            sem_post(&serve);
+        } 
+        else{
+            sem_post(&eat);
+            //printf("Bees: Im so tired, somebody else gotta work now\n"); //check if sleep works
+            sleep(2);   
+        }
+    }
+}
+
+//babybird function
+void *bear(){
+    
+    while(true){
+        sem_wait(&serve);
+        if(honeypot < 0)
+            honeypot = 0;
+
+        if(honeypot == 0){
+            printf("Bees: WHAT!? The honey is gone!!!!! \n");
+            printf("ʕ•ᴥ•ʔ: zzzzzzzzzZZZzzzzZZZzzzZZZ \n");
+            sem_post(&eat);
+        }
+        else{
+            honeypot -= 5;
+            printf("ʕ•ᴥ•ʔ: SLURP SLURP I love honey\n honey left: %d\n", honeypot);
+            sem_post(&serve);
+        }
+    }
+}
+
+int main(){
+    int id[WORKERS-1];
+    sem_init(&serve, SHARED, 1);
+    sem_init(&eat, SHARED, 0);
+    pthread_t theBeeCrew[WORKERS-1]; //beees
+    pthread_t Bear; //the hungry bear
+
+    for(int i=0;i < WORKERS-1;i++){
+        id[i] = i;
+	    pthread_create(&theBeeCrew[WORKERS-1], NULL, bees, (void *) &id[i]);
+    }  
+    
+	pthread_create(&Bear,NULL,bear, NULL);
+    
+
+    for(int i = 0; i < WORKERS-1; i++){
+    	pthread_join(theBeeCrew[i], NULL);
+    }
+    pthread_join(Bear, NULL);     
+    return 0;
+}
