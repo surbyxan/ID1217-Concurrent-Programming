e1fedf0996f8ebfb5309e6593d9b3118b9ca32b7
Jonathan <jcullgert@hotmail.se>
2025-02-18 16:34:55 +0100

    probably done with task 1

--- HomeWork5/task.c
+++ HomeWork5/task.c
@@ -10,20 +10,17 @@
 #define students n
 
 int main(int argc, char *argv[]){
-    MPI_Init(&argc, &argv);
-    //MPI_MPI_COMM_WORLD MPI_COMM_WORLD;
 	int Id, partner, nofstudents;
-	bool lonely = true;//mefrfr
+    MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &Id );
 	MPI_Comm_size(MPI_COMM_WORLD, &nofstudents);
-	//MPI_MPI_COMM_WORLD_somt(somth, &partner);
+	
 
 	//TEACHER
     if(Id == TEACHER){
         int screamingStudents[nofstudents - 1];
 		int index = 0;
         printf(" partner up boy\n");
-        //rbuf = some shit
 		//Getting requests from students
         for(int i = 1; i < nofstudents; i++){
             MPI_Recv(&screamingStudents[index], 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -32,27 +29,26 @@ int main(int argc, char *argv[]){
 		//Paring students
         for(int i = 0; i < nofstudents - 1; i++){
 			if(i == nofstudents - 2 && nofstudents%2 == 0){
-                //this is the last student and it is forever alone :(
+                //this is the last student and it is forever alone :´C
                 MPI_Send(&screamingStudents[i], 1, MPI_INT, screamingStudents[i], 0, MPI_COMM_WORLD);
             } 
-            else if(i % 2 == 0){
-                MPI_Send(&screamingStudents[i], 1 , MPI_INT, screamingStudents[i++],0 , MPI_COMM_WORLD);//men den skickar id av i till i++
-            }
-            else if(i % 2 != 0){
-                MPI_Send(&screamingStudents[i] , 1 ,MPI_INT, screamingStudents[i--], 0 , MPI_COMM_WORLD);//skickar id av i till i--
-            }
-	
-            
+            else if (i % 2 == 0){ 
+                    //student pairing, the teacher sends the pairs to the students.
+                    MPI_Send(&screamingStudents[i + 1], 1, MPI_INT, screamingStudents[i], 0, MPI_COMM_WORLD);
+                    MPI_Send(&screamingStudents[i], 1, MPI_INT, screamingStudents[i + 1], 0, MPI_COMM_WORLD);
+                    i++;
+            }   
         }
 	}
-    else{
+    else{//STUDENT 
+        //The student sends a partner request and recives a partner from teacher
         MPI_Send(&Id, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        MPI_Recv(&partner, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&partner, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //When the student have gotten a partner it prints its ID and the partners ID
         printf("HI, I'm %d and my partner is %d\n", Id, partner);
 
     }	
-	
-	//när ihopparningen är klar
+	//ending the MPI process part
     MPI_Finalize();
     return 0;
 }
